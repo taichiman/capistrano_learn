847bb13b21c9ba4bf04eb71bd89cfff22581171c
Denis Krivostchokov <densomart@gmail.com>
2013-11-19 17:00:32 +0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapistranoLearn::Application.config.secret_token = 'dff6cbc5337aeb36ad88b6fb03e6994b990a0e7964f5bb4bec959802901069cc67e9f6d2b902845d6ce0f405dd460a1966c805bd6bb0ddb783c2f2fa4e40cad3'
